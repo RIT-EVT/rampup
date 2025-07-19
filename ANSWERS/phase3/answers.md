cbdce821176adfed7affe20188109a036677eee9
tmb5932 <tmb5932@g.rit.edu>
2025-07-19 04:03:12 -0400

    added answers for phase 3

---
+++ ANSWERS/phase3/answers.md
@@ -0,0 +1,25 @@
+# Phase 3 Answers
+Phase 3 is about learning basics of I2C communication and driver development.
+
+## targets/phase3/main.cpp
+The main things that need to be seen in here is just that they:
+- Create/Setup I2C `i2c = IO::getI2C<...>()`
+- Create/Setup the TMP117 `tmp(...)`
+- Call the readTemp(...) `tmp.readTemp(temp)`
+- Print result
+
+Look at main.cpp for completed implementation.
+
+## src/dev/TMP117.cpp readTemp() Implementation
+The main things that need to be checked in the code is that they are:
+- Reading from i2c `i2c.read(...)`
+- Combining the raw output from the sensor `(outputBuffer[0] << 8) | outputBuffer[1]`
+- Converting raw output into either centi-celsius (preferred) or celsius `rawOutput * 78125 / 10000 or 1000000`.
+
+Look at TMP117.cpp for completed implementation.
+
+## Expected UART output
+IMAGE TO BE ADDED!
+
+## Saleae Expected Image
+IMAGE TO BE ADDED!
---
+++ ANSWERS/phase3/main.cpp
@@ -0,0 +1,45 @@
+/**
+ * This is a simple example of reading data over I2C and printing it over UART.
+ */
+#include "dev/TMP117.hpp"
+#include <EVT/io/I2C.hpp>
+#include <EVT/io/UART.hpp>
+#include <EVT/manager.hpp>
+
+using namespace std;
+namespace IO = EVT::core::IO;
+namespace time = EVT::core::time;
+constexpr uint8_t i2cSlaveAddress = 0x48; // This is known by looking at the click board and table in datasheet, depends on what the ADD0 pin is connected to (GND, 3.3V, SDA, SCL)
+
+int main() {
+    // Initialize system
+    EVT::core::platform::init();
+
+    // Set up UART
+    IO::UART& uart = IO::getUART<IO::Pin::UART_TX, IO::Pin::UART_RX>(9600);
+
+    // Set up I2C
+    IO::I2C& i2c = IO::getI2C<IO::Pin::PB_8, IO::Pin::PB_9>();
+
+    // Create TMP117 instance
+    rampup::TMP117 tmp(i2c, i2cSlaveAddress);
+
+    uint16_t temperature;
+
+    while (1) {
+        // Read temperature
+        IO::I2C::I2CStatus status = tmp.readTemp(temperature);
+
+        // Error handling
+        if (status != IO::I2C::I2CStatus::OK) {
+            uart.printf("I2C Error!");
+            continue;
+        }
+
+        // Print temperature as normal celsius (2534 centi-Celsius => 25.34 Celsius)
+        uart.printf("Temp: %d.%d Celsius\n\r", temperature / 100, temperature % 100);
+
+        // Wait 1 s
+        time::wait(1000);
+    }
+}
