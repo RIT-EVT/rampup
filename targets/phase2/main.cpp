c48fec3310f4a7fbad779b9496cbde17b1a79f4b
DiegoLHendrix <diegolhendrix@gmail.com>
2025-07-20 15:00:40 -0500

    Phase 2 answers added and TMP117.cpp edited to use new namespaces

--- targets/phase2/main.cpp
+++ targets/phase2/main.cpp
@@ -1,19 +1,42 @@
 /**
  * This is a simple example of taking user input and printing output over UART.
  */
+#include <core/io/UART.hpp>
+#include <core/io/pin.hpp>
+#include <core/manager.hpp>
+
+namespace io = core::io;
 
 int main() {
     // Initialize system
+    core::platform::init();
 
     // Set up UART
+    io::UART& uart = io::getUART<io::Pin::UART_TX, io::Pin::UART_RX>(9600);
 
     // Declare two arrays of characters to store user input
+    char userInput1[100];
+    char userInput2[100];
+
+    uint64_t num1 = 0;
+    uint64_t num2 = 0;
+    uint64_t num3 = 0;
 
     while (1) {
         // Read user input
+        uart.printf("\n\rEnter first number: ");
+        uart.gets(userInput1, 100);
+
+        uart.printf("\n\rEnter second number: ");
+        uart.gets(userInput2, 100);
 
         // Convert the two character arrays to uint64_t
+        num1 = static_cast<uint64_t>(userInput1[0] - '0');
+        num2 = static_cast<uint64_t>(userInput2[0] - '0');
+
+        num3 = num1 + num2;
 
         // Output the sum of the two numbers
+        uart.printf("\n\r%u\n\r", num3);
     }
 }
