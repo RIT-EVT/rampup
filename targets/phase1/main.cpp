043d371d398255f04d45f2f1102c3295143b58d0
tmb5932 <tmb5932@g.rit.edu>
2025-08-12 21:21:25 -0400

    woops didnt mean to have that in there

--- targets/phase1/main.cpp
+++ targets/phase1/main.cpp
@@ -11,23 +11,7 @@ int main() {
     core::platform::init();
 
     // Setup GPIO
-    io::GPIO& gpio = io::getGPIO<io::Pin::PA_5>(io::GPIO::Direction::OUTPUT, io::GPIO::Pull::PULL_DOWN);
-
-    io::GPIO::State state = io::GPIO::State::HIGH;
 
     // Loop and toggle LED on and off; waiting .5 seconds after each toggle
-    while (1) {
-        // Update GPIO output
-        gpio.writePin(state);
 
-        // Swap state HIGH/LOW
-        if (state == io::GPIO::State::HIGH) {
-            state = io::GPIO::State::LOW;
-        } else {
-            state = io::GPIO::State::HIGH;
-        }
-
-        // Wait .5 seconds
-        core::time::wait(500);
-    }
 }
