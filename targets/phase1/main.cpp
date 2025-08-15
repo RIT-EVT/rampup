8fc38cceb5edb6072bb683402d89a9587ebf2761
tmb5932 <tmb5932@g.rit.edu>
2025-08-15 00:03:27 -0400

    wrote probable solution code for phase1

--- targets/phase1/main.cpp
+++ targets/phase1/main.cpp
@@ -1,5 +1,34 @@
 /**
  * This is a simple example of GPIO usage
  */
+#include <core/manager.hpp>
 
-int main() {}
+namespace io = core::io;
+namespace time = core::time;
+
+int main() {
+    // Initialize system
+    core::platform::init();
+
+    // Setup GPIO pin for LED
+    io::GPIO& led = io::getGPIO<io::Pin::PA_1>();
+
+    // variable to store state in
+    io::GPIO::State state = io::GPIO::State::HIGH;
+
+    // Loop infinitely while toggling the LED on/off
+    while (true) {
+        // Toggle the LED on/off
+        led.writePin(state);
+
+        // Swap state to other option
+        if (state == io::GPIO::State::HIGH) {
+            state = io::GPIO::State::LOW;
+        } else {
+            state = io::GPIO::State::HIGH;
+        }
+
+        // Wait .5 seconds
+        time::wait(500);
+    }
+}
