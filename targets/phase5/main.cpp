cab36e67365bf657eeaadb0f6661e1b15a9e1fa4
Ethan-Caracoglia <ethancaracoglia@gmail.com>
2025-08-18 11:00:49 -0400

    Write-up rough draft is finished

--- targets/phase5/main.cpp
+++ targets/phase5/main.cpp
@@ -1,4 +1,15 @@
 /**
  * This is a simple example of ADCs
  */
-int main() {}
+#include <core/io/UART.hpp>
+#include <core/io/ADC.hpp>
+#include <core/io/pin.hpp>
+#include <core/manager.hpp>
+
+namespace io = core::io;
+
+int main() {
+
+
+
+}
